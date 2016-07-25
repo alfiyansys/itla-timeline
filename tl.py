cabdc0309922d4c9198149f1c907ead9c21dcf75
alfiyansys <alfiyan.syamsuddin@gmail.com>
2016-07-25 15:49:56 +0700

    timeline finished

--- tl.py
+++ tl.py
@@ -5,6 +5,25 @@ k = 3
 h = 60
 
 ba = m + k + h + k
-print ba
+print "lama fase " + str(ba) + " detik "
+
+def stateTL(dt):
+    cpmk = m
+    cpkh = m + k
+    cphk = m + k + h
+    cpkm = m + k + h + k
+
+    if dt >= 0 and dt < cpmk:
+        return "merah"
+    elif dt >= cpmk and dt < cpkh:
+        return "kuning"
+    elif dt >= cpkh and dt < cphk:
+        return "hijau"
+    elif dt >= cphk and dt < cpkm:
+        return "kuning"
+    else:
+        return "udv"
+
+
 for num in range(0,ba):
-    print num
+    print str(num) + " = " + stateTL(num)

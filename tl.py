f518170db614a8aa679c2aefb7d912c48635f3f7
alfiyansys <alfiyan.syamsuddin@gmail.com>
2016-07-25 16:21:04 +0700

    Stable timeline loop + multi threading

--- tl.py
+++ tl.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python
 
-m = 30
-k = 3
-h = 60
+import time, threading
+
+m = 5
+k = 1
+h = 5
 
 ba = m + k + h + k
 print "lama fase " + str(ba) + " detik "
+
+def process():
+    print "Melakukan proses #process"
+    time.sleep(3) #pemrosesan video ifal ada di sini, tidak mengganggu timeline
+    print "Proses selesai #process"
+    return 0
 
 def stateTL(dt):
     cpmk = m
@@ -13,17 +21,33 @@ def stateTL(dt):
     cphk = m + k + h
     cpkm = m + k + h + k
 
+    print "m = " + str(m) + ", k = " + str(k) + ", h = " + str(h)
+
     if dt >= 0 and dt < cpmk:
         return "merah"
     elif dt >= cpmk and dt < cpkh:
         return "kuning"
     elif dt >= cpkh and dt < cphk:
-        return "hijau"
+        if procthread.isAlive():
+            print "masih melakukan proses di detik "+str(dt)
+        else:
+            procthread.start()
+        return "hijau | Lakukan proses di sini"
     elif dt >= cphk and dt < cpkm:
         return "kuning"
     else:
         return "udv"
 
-
-for num in range(0,ba):
-    print str(num) + " = " + stateTL(num)
+procthread = threading.Thread(target=process)
+print "======================= START ======================="
+while 1:
+    for num in range(0,ba):
+        print "detik: " + str(num)
+        if procthread.isAlive():
+            print "thread aktif #main"
+        else:
+            procthread = threading.Thread(target=process)
+        print "state = " + stateTL(num)
+        print ""
+        time.sleep(1)
+    print "============= Restart Fase ==============="
